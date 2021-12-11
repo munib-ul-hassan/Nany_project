4e5ded74d0adcc5d375924bac8fa47403f3c11ef
Munib <munibkhatri123@gmail.com>
2021-12-11 20:45:36 +0500

    resolve error

--- src/api/Topheader.js
+++ src/api/Topheader.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const topheader = require("../models/topheader");
+const topheader = require("../models/Topheader");
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
