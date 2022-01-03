d8f4d6db3239863914d28e10d429168781c44427
munib <munibkhatri123@gmail.com>
2022-01-02 21:34:49 -0800

    order API invoice edit and send

--- src/api/Order.js
+++ src/api/Order.js
@@ -2,38 +2,43 @@ const express = require("express");
 const router = express.Router();
 const Order = require("../models/Order");
 const nodemailer = require("nodemailer");
+const {PDFNet} = require('@pdftron/pdfnet-node')
+const path = require('path')
+const fs = require('fs')
+
 const { verifyadmintoken, verifytoken } = require('../middleware/auth')
 
 router.post("/", async (req, res) => {
   try {
     const { order, product } = req.body;
+    // console.log(order,product);
     var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if (!(order && product)) {
       res
         .status(200)
         .send({ message: "All input is required", success: false });
     } else
-      if (!re.test(email)) {
+      if (!re.test(order.email)) {
         res.status(422).send({ message: "invlaid Email", success: false });
       } else {
         const count = 0
-        product.map((item, index) => {
-          product[index] = Object.assign(order, {
-            product: item._id,
-            quantity: item.quantity,
-            price: item.price,
-            color: item.color
-          })
-          product[index].status = "Pending"
+        // product.map((item, index) => {
+        //   product[index] = Object.assign(order, {
+        //     product: item._id,
+        //     quantity: item.quantity,
+        //     price: item.price,
+        //     color: item.color
+        //   })
+        //   product[index].status = "Pending"
 
-          const Booking = new Order(product[index]);
-          Booking.save().then((item) => {
-            if (item) {
-              count++;
-            } else {
-            }
-          })
-        })
+        //   const Booking = new Order(product[index]);
+        //   Booking.save().then((item) => {
+        //     if (item) {
+        //       count++;
+        //     } else {
+        //     }
+        //   })
+        // })
         ///////////////////////////////
         // let testAccount = await nodemailer.createTestAccount();
         // let transporter = nodemailer.createTransport({
@@ -59,6 +64,35 @@ router.post("/", async (req, res) => {
         // // Preview only available when sending through an Ethereal account
         // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
         ///////////////////////////////
+
+        const inputpath = path.resolve(__dirname,'../../file/Assignment01.pdf')
+        const outputpath = path.resolve(__dirname,'../../file/Invoice.pdf')
+  const textreplace =async()=>{        
+      const pdfDoc = await PDFNet.PDFDoc.createFromFilePath(inputpath);
+      await pdfDoc.initSecurityHandler();
+      const replacer = await PDFNet.ContentReplacer.create();
+      const page = await pdfDoc.getPage(1)
+      
+      await replacer.addString("Question","Munib")
+      // await replacer.addString("Name","Munib")
+      // await replacer.addString("Name","Munib")
+      // await replacer.addString("Name","Munib")
+      await replacer.process(page)
+      pdfDoc.save(outputpath,PDFNet.SDFDoc.SaveOptions.e_linearized)
+      
+}
+
+PDFNet.runWithCleanup(textreplace,"demo:1641127981940:7b5ef390030000000067b17cad66d1fa23446ceda8e6cef1d73fff0305").then(()=>{
+  fs.readFile(outputpath,(err,data)=>{
+    if(err){
+    console.log(err);
+    }else{
+      console.log(data);
+    }
+  })
+})
+
+
         if (count == product.length) {
           res.status(200).send({
             message: "Data save into Database",
